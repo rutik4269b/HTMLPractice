5f6769e243efa1334e169d7b7517971c0732f5a5
Rutik Bhandalkar <87383574+rutik4269b@users.noreply.github.com>
2024-10-16 20:21:48 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,24 @@
+fullname = "Rutik Bhandalkar";
+console.log(fullname);
+age = 24;
+console.log(age);
+x = null;
+y = undefined;  
+console.log("welcome Rutik Bhandalkar!");
+console.log("Hello World");
+const Student = {
+    fullname :"Rutik Bhandalkar",
+    age :" 24",
+    Cgpa : "8.13",
+    isPass : true,
+    
+};
+
+const Profile = {
+    username: "@rutikb",
+    isFollow: true,
+    followers : 990,
+    following : 2000,
+
+};
+console.log(Profile);    
